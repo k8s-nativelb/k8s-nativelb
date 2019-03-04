a46ca95a9985bd1032059202f1127748a7f65a10
Sebastian Sch <sebassch@gmail.com>
2019-03-04 17:21:06 +0200

    Fix travis build

--- hack/goveralls.sh
+++ hack/goveralls.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-goveralls -coverprofile=cover.out -service=travis-ci -repotoken ${COVERALLS_TOKEN} -ignore=$(find -regextype posix-egrep -regex ".*generated_mock.*\.go|.*swagger_generated\.go|.*openapi_generated\.go" -printf "%P\n" | paste -d, -s)
+TRAVIS_JOB_ID=${TRAVIS_JOB_ID} TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST} TRAVIS_BRANCH=${TRAVIS_BRANCH} goveralls -coverprofile=cover.out -ignore=$(find -regextype posix-egrep -regex ".*generated_mock.*\.go|.*swagger_generated\.go|.*openapi_generated\.go" -printf "%P\n" | paste -d, -s)
--- hack/run.sh
+++ hack/run.sh
@@ -4,6 +4,9 @@ docker run --rm -t --user $(id -u):$(id -g) \
            --network host \
            --volume `pwd`:/go/src/github.com/k8s-nativelb \
            --env KUBECONFIG=/go/src/github.com/k8s-nativelb/cluster/dind-cluster/config \
-           --env COVERALLS_TOKEN="${COVERALLS_TOKEN}" \
+           --env COVERALLS_REPO_TOKEN=${COVERALLS_REPO_TOKEN} \
+           --env TRAVIS_JOB_ID=${TRAVIS_JOB_ID} \
+           --env TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST} \
+           --env TRAVIS_BRANCH=${TRAVIS_BRANCH} \
            --workdir /go/src/github.com/k8s-nativelb/ \
            quay.io/k8s-nativelb/base-image:latest make $@
